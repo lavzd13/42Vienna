e6e6eddc73dd9c5f14e3fa72e0223e366672e6c0
Jovan Lomic <jlomic@c2r7p6.42vienna.com>
2023-09-21 22:24:00 +0200

    Added finished printf with grade 100

---
+++ ft_printf/libft/ft_lstadd_back.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlomic <jlomic@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/11 19:38:23 by jlomic            #+#    #+#             */
+/*   Updated: 2023/09/13 13:00:30 by jlomic           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*temp;
+
+	temp = ft_lstlast(*lst);
+	if (!temp)
+		*lst = new;
+	else
+		temp->next = new;
+}
