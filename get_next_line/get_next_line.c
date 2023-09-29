4f893c8cb37177a31a91761e3b8713d3501a547b
Jovan Lomic <jlomic@c4r2p8.42vienna.com>
2023-09-29 14:05:06 +0200

    Added get_next_line with bonuses

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlomic <jlomic@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/25 17:26:25 by jlomic            #+#    #+#             */
+/*   Updated: 2023/09/27 22:29:02 by jlomic           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_joinfree(char *buff, char *temp)
+{
+	char	*tmp;
+
+	tmp = ft_strjoin(buff, temp);
+	free(buff);
+	return (tmp);
+}
+
+int	ft_bytecheck(ssize_t bytes, char *buffer, char *temp)
+{
+	if (bytes == -1)
+	{
+		free(buffer);
+		free(temp);
+		return (0);
+	}
+	else
+		return (1);
+}
+
+char	*ft_new(char *buffer)
+{
+	int		i;
+	char	*temp;
+
+	i = 0;
+	if (!buffer[i])
+		return (NULL);
+	while (buffer[i] && buffer[i] != '\n')
+		++i;
+	if (buffer[i] == '\n')
+		i += 2;
+	i++;
+	temp = malloc(i * sizeof(char));
+	if (!(smallcheck(temp, buffer)))
+		return (NULL);
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		temp[i] = buffer[i];
+		++i;
+	}
+	if (buffer[i] && buffer[i] == '\n')
+		temp[i++] = '\n';
+	temp[i] = '\0';
+	return (temp);
+}
+
+char	*ft_read(char *buffer, int fd)
+{
+	char	*temp;
+	ssize_t	bytesread;
+
+	if (!buffer)
+	{
+		buffer = malloc(1);
+		*buffer = 0;
+	}
+	bytesread = 1;
+	temp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!temp)
+		return (NULL);
+	while (bytesread)
+	{
+		bytesread = read(fd, temp, BUFFER_SIZE);
+		if (!(ft_bytecheck(bytesread, buffer, temp)))
+			return (NULL);
+		temp[bytesread] = '\0';
+		buffer = ft_joinfree(buffer, temp);
+		if (ft_strchr(temp, '\n'))
+			break ;
+	}
+	free(temp);
+	return (buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = ft_read(buffer, fd);
+	if (buffer)
+	{
+		line = ft_new(buffer);
+		buffer = ft_sisaj(buffer);
+		return (line);
+	}
+	else
+		return (NULL);
+}
+
+/* #include <stdio.h>
+#include <fcntl.h>
+
+int	main()
+{
+	int		fd = open("test.txt", O_RDWR);
+	int		i = 0;
+	char	*tmp;
+
+	while (i < 1)
+	{
+		tmp = get_next_line(fd);
+		printf("%s", tmp);
+		free(tmp);
+		i++;
+	}
+} */
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlomic <jlomic@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/25 11:41:03 by jlomic            #+#    #+#             */
+/*   Updated: 2023/09/25 20:14:42 by jlomic           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	smallcheck(char *temp, char *buffer)
+{
+	if (!temp)
+	{
+		free(buffer);
+		return (0);
+	}
+	else
+		return (1);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	str = (char *)s;
+	while (str[i] != '\0')
+	{
+		if (str[i] == (unsigned char)c)
+			return (str + i);
+		++i;
+	}
+	if (str[i] == (unsigned char)c)
+		return (str + i);
+	return (0);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*total;
+	size_t	size;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (!s1 && !s2)
+		return (0);
+	size = ft_strlen(s1) + ft_strlen(s2);
+	total = (char *)malloc((size + 1) * sizeof(char));
+	if (!total)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		total[i] = s1[i];
+		++i;
+	}
+	while (s2[j] != '\0')
+	{
+		total[i + j] = s2[j];
+		++j;
+	}
+	total[size] = '\0';
+	return (total);
+}
+
+char	*ft_sisaj(char *buffer)
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 0;
+	j = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		++i;
+	if (!buffer[i])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	temp = malloc(sizeof(char) * (ft_strlen(buffer) - i + 1));
+	if (!temp)
+		return (NULL);
+	i++;
+	while (buffer[i])
+		temp[j++] = buffer[i++];
+	temp[j] = '\0';
+	free(buffer);
+	return (temp);
+}
+/*  #include <stdio.h>
+int	main()
+{
+	char *s = "First line\nsecond line\nThird line";
+	char *str = ft_strtrim(s, 34);
+	printf("First: %s\n", str);
+	str = ft_strtrim(str, 23);
+	printf("Second: %s", str);
+}
+ */
